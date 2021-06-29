49eaec4683c6b117d574f87f8f8ca9a585400c11
pranay2594 <pranaynimkar7@gmail.com>
2021-06-29 23:14:44 +0530

    --- Auto-Git Commit ---

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
-Hello world
+provider "aws" {
+    region = "us-east-1"
+}
+resource "aws_instance" "myterraforminstance" {
+    ami = "ami-0ab4d1e9cf9a1215a"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "myterraforminstance"
+    }
+}
